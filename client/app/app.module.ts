3263d8144d7e4dfe2dddb60fc0423b13b2b7132f
Alex Matos da Silva <alexmatos.br@gmail.com>
2017-04-10 16:56:01 -0400

    painel

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import {FotoModule} from './foto/foto.module';
-import {HttpModule} from '@angular/http';
 import 'rxjs/add/operator/map';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { HttpModule } from '@angular/http';
+
+import { PainelModule } from './painel/painel.module';
 
 @NgModule({
-    imports: [ BrowserModule, FotoModule, HttpModule ],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
