f52f1438e175797a98429f4e106b452e7e14d745
Alex Matos da Silva <alex.matos@ufms.br>
2017-04-18 16:51:50 -0400

    Até penúltima aula

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -7,4 +9,26 @@ import {Component, Input} from '@angular/core';
 })
 export class CadastroComponent {
 
+    foto: FotoComponent = new FotoComponent();
+    htpp: Http;
+
+    constructor(http: Http) {
+        this.htpp = http;
+    }
+
+    cadastrar($event) {
+        event.preventDefault();
+        console.log(this.foto);
+
+        let header = new Headers();
+        header.append('Content-type', 'aplication/json');
+        this.htpp
+            .post('v1/fotos', JSON.stringify(this.foto), { headers: header })
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                console.log("Foto salva com sucesso!");
+            }, erro => 
+                console.log(erro)
+            );
+    }
 }
